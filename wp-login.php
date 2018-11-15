4bc5a9035b28b1a8778e22319c431fafcb622d44
m7131 <45068223+m7131@users.noreply.github.com>
2018-11-15 19:19:42 +0300

    Fix CSS

--- wp-login.php
+++ wp-login.php
@@ -9,7 +9,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Войти &lsaquo; Наш новый проект &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' href='/https://m7131.github.io/SiteMy/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
+<link rel='stylesheet' href='http://localhost/https:/m7131.github.io/SiteMy/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
 <meta name='robots' content='noindex,follow' />
 	<meta name="viewport" content="width=device-width" />
 		</head>
@@ -17,7 +17,7 @@
 		<div id="login">
 		<h1><a href="https://ru.wordpress.org/" title="Сайт работает на WordPress" tabindex="-1">Сайт работает на WordPress</a></h1>
 	
-<form name="loginform" id="loginform" action="/https://m7131.github.io/SiteMy/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="http://localhost/https:/m7131.github.io/SiteMy/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Имя пользователя или e-mail<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -35,7 +35,7 @@
 </form>
 
 <p id="nav">
-	<a href="/https://m7131.github.io/SiteMy/wp-login.php?action=lostpassword">Забыли пароль?</a>
+	<a href="http://localhost/https:/m7131.github.io/SiteMy/wp-login.php?action=lostpassword">Забыли пароль?</a>
 </p>
 
 <script type="text/javascript">
@@ -52,7 +52,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="/https://m7131.github.io/SiteMy/">&larr; Назад к сайту &laquo;Наш новый проект&raquo;</a></p>
+	<p id="backtoblog"><a href="http://localhost/https:/m7131.github.io/SiteMy/">&larr; Назад к сайту &laquo;Наш новый проект&raquo;</a></p>
 		
 	</div>
 
